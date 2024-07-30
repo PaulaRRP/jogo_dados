fda7b99576a1e641f89880e9c0a3cee023f95ff0
Paula Rinco <44785516+PaulaRRP@users.noreply.github.com>
2024-07-30 02:05:00 +0000

    criando loop

--- main.hs
+++ main.hs
@@ -1,4 +1,5 @@
 import System.Random (randomRIO)
+import Data.Maybe (fromMaybe)
 
 -- Define um novo tipo Dado, que é um wrapper para Int
 newtype Dado = Dado Int deriving (Show)
@@ -12,6 +13,7 @@ main = do
     listaDeDados <- criarListaDeDados numDados
     putStrLn ("Seu jogo será iniciado com " ++ show (length listaDeDados) ++ " dados.")
     putStrLn ("Lista de dados: " ++ show listaDeDados)
+    realizaJogadas listaDeDados
 
 -- Função para criar uma lista de Dados com valores aleatórios
 criarListaDeDados :: Int -> IO [Dado]
@@ -22,3 +24,27 @@ gerarDadoAleatorio :: IO Dado
 gerarDadoAleatorio = do
     valor <- randomRIO (1, 6)
     return (Dado valor)
+
+-- Função recursiva para realizar jogadas
+realizaJogadas :: [Dado] -> IO ()
+realizaJogadas [] = putStrLn "Todos os dados foram removidos. Jogo encerrado."
+realizaJogadas dados = do
+    putStrLn "Escolha o número do dado a ser removido (1 a N):"
+    let listaIndices = zip [1..] dados
+    mapM_ (\(i, dado) -> putStrLn (show i ++ ": " ++ show dado)) listaIndices
+    input <- getLine
+    let escolha = read input :: Int
+    if escolha >= 1 && escolha <= length dados
+        then do
+            let dadoEscolhido = dados !! (escolha - 1)
+            let dadosAtualizados = removeDado dados (escolha - 1)
+            putStrLn ("Dado removido: " ++ show dadoEscolhido)
+            putStrLn ("Lista de dados atualizada: " ++ show dadosAtualizados)
+            realizaJogadas dadosAtualizados
+        else do
+            putStrLn "Escolha inválida. Tente novamente."
+            realizaJogadas dados
+
+-- Função para remover um dado da lista com base no índice
+removeDado :: [Dado] -> Int -> [Dado]
+removeDado dados idx = take idx dados ++ drop (idx + 1) dados
